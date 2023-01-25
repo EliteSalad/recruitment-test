c63bcae7ffb3629c01461d11c9d2cbc9833e5759
EliteSalad <32502593+EliteSalad@users.noreply.github.com>
2023-01-25 23:12:19 +0000

    code cleaned up

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -9,16 +9,17 @@ export default class App extends Component {
         this.state = {
             items: [],
             isLoaded: false,
+            newEmployee: 'employee name',
+            newEmployeeValue: '12345'
         }
-        this.headers = [
-            { key: 'id', label: 'Id' },
-            { key: 'id', label: 'Employee' },
-            { key: 'id', label: 'Value' }
-        ]
+
+        this.AddEmployee = this.AddEmployee.bind(this);
+        this.ModifyEmployee = this.ModifyEmployee.bind(this);
+        this.RemoveEmployee = this.RemoveEmployee.bind(this);
+
     }
 
-
-    componentDidMount() {
+    componentDidMount = () => {
         fetch('http://localhost:41478/employees')
             .then(res => res.json()) 
             .then(json => {
@@ -31,24 +32,36 @@ export default class App extends Component {
     }
     
 
-    //componentDidMount() {
-    //    fetch('http://localhost:41478/employees')
-    //    .then(function (result) {
-    //        return result.text();
-    //    })
-    //    .then(function (result) {
-    //        console.log('here is our content:', result);
-    //    })
-    //    .catch(function (err) {
-    //        console.error(err.message);
-    //    })
-    //}
+    AddEmployee = (event) => {
+        event.preventDefault();
+        console.log("SaveNewNameCalled");
+        console.log(this.state.items);
+        this.state.setState({
+            items: [...this.items, { name: "test", value: 2318 }],
+        });
+        console.log(this.state.items);
+    }
+
+    ModifyEmployee = (event) => {
+        event.preventDefault();
+        console.log("SaveNewNameCalled");
+    }
+
+    RemoveEmployee = (event) => {
+        event.preventDefault();
+        console.log("SaveNewNameCalled");
+    }
+
+
+
 
     render() {
-        const HandleClick = () => {
-            console.log("buttons work");
-        }
-        var { isLoaded, items } = this.state;
+        var { isLoaded, items, newEmployee, newEmployeeValue } = this.state;
+        //this.setState({
+        //    items: [...items, { name: "test", value: 2318 }],
+        //});
+
+
 
         if (!isLoaded) { 
         return <div>Loading...</div>;
@@ -56,50 +69,35 @@ export default class App extends Component {
             else {
                 return (<div className="App">
                     <h1>Data has been loaded</h1>
-                    <form>
-                        <input type="text" name="text" id="text" required/>
-                            <button type="submit" class="btn-list">
-                            </button>
+                    <form onSubmit>
+                        <div>Employee And Value</div>
+                        <input type="text" value={newEmployee} name="text" id="text" required  />
+                        <input type="number" value={newEmployeeValue}  name="number" id="number" required />
+                        <button type="submit" name="btn-list" id="btn-list" onClick={this.AddEmployee.bind(this)}>Add</button>
+                        <button name="btn-list" id="btn-list" onClick={this.RemoveEmployee.bind(this)}>Remove</button>
+                        <button name="btn-list" id="btn-list" onClick={this.ModifyEmployee.bind(this)}>Modify</button>
+                        <input type="submit" />
                     </form>
+                    <div id="container">
                     <ul>
-                        {items.map(item => (
-                            <><li key={item.name}>
-                                {item.name}  {item.value}
-                            </li><input type="checkbox" className="selectsingle" value="1" /></>
-                            ))}
-                    </ul>
+                        {
+                                items.map((item, index) => 
+                                    <li key={index}>
+                                        {item.name} {item.value}
+                                        <input type="checkbox"></input>
+                                    </li>
+                                    
+                            )
+                        }
+                        </ul>
+
+                    </div>
                 </div>)
         };
         
     return (
         <body>
-            <div className="container"><h1>Testing 1 2 3 </h1>
-                <button tpye="button" className="btn btn-danger">Delete Selected Employee</button>
-                <button onClick={HandleClick} tpye="button" className="btn btn-danger">Load Employees</button>
-            <table className="table table-bordered table-striped">
-            <thread>
-                <tr>
-                    {   this.headers.map(function (h) {
-                            return (
-                                <th key={h.key}>{h.label}</th>
-                            )
-                        })
-                    }
-                </tr>
-                    </thread>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <input type="checkbox" className="selectsingle" value="1" />
-                                1
-                            </td>
-                            <td>Xavier</td>
-                            <td>420</td>
-                        </tr>
-                    </tbody>
-            </table>
-            </div>
-
+ 
         </body>
     );
   }
